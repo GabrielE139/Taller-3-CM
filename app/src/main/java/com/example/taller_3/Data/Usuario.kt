cee051913d806061898cdd2675e59b5ef67190d8
Gabriel13 <gabrielesp7724@gmail.com>
2024-11-02 16:16:45 -0500

    Pequeños cambios #1

---
+++ app/src/main/java/com/example/taller_3/Data/Usuario.kt
@@ -0,0 +1,10 @@
+package com.example.taller_3.Data
+
+data class Usuario(
+    var nombre: String,
+    var apellido: String,
+    var email: String,
+    var password: String,
+    var num_id: Double,
+    var latitud: Double,
+    var longitud: Double)
