aa1c071fe61b89bb17278b9df093f8e98632c6df
Andrés Ruge <119058759+Rujnp794@users.noreply.github.com>
2024-11-06 14:51:08 -0500

    Menu con problema en el mapa

--- app/src/main/java/com/example/taller_3/Logica/Inicio.kt
+++ app/src/main/java/com/example/taller_3/Logica/Inicio.kt
@@ -2,15 +2,24 @@ package com.example.taller_3.Logica
 
 import android.Manifest
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.MatrixCursor
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.taller_3.Data.Datos
+import com.example.taller_3.Data.Usuario
 import com.example.taller_3.R
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -20,6 +29,13 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import org.json.JSONObject
 import java.io.InputStream
 
@@ -32,10 +48,16 @@ class Inicio : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
     private var isCameraMoved = false
+    private lateinit var auth: FirebaseAuth
+    private val database = FirebaseDatabase.getInstance()
+    private lateinit var myRef: DatabaseReference
+    private val PATH_USERS = "users/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+
 
         mapa = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapa.view?.visibility = View.GONE
@@ -62,6 +84,7 @@ class Inicio : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
         }
+        escucharCambiosdeDisponibilidad()
     }
 
     private fun pedirPermiso(context: Activity, permisos: Array<String>, justificacion: String, idCode: Int) {
@@ -166,5 +189,100 @@ class Inicio : AppCompatActivity(), OnMapReadyCallback {
         }
 
         return locations
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.menuLogOut -> {
+
+                FirebaseApp.initializeApp(this)
+                auth = FirebaseAuth.getInstance()
+                auth.signOut()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+                true
+            }
+            R.id.EstablecerEstado -> {
+                val userId = auth.currentUser?.uid
+                if (userId != null) {
+                    val database = FirebaseDatabase.getInstance().reference
+                    val userStatusRef = database.child("users").child(userId).child("disponible")
+
+                    userStatusRef.get().addOnSuccessListener { snapshot ->
+                        val isAvailable = snapshot.getValue(Boolean::class.java) ?: false
+                        val newStatus = !isAvailable
+                        userStatusRef.setValue(newStatus).addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val message = if (newStatus) {
+                                    "Ahora estás disponible"
+                                } else {
+                                    "Ahora estás desconectado"
+                                }
+                                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this, "Error al actualizar el estado", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                }
+                true
+            }
+            R.id.IrAUsuarios -> {
+                val intent = Intent(this, UsuariosDisponibles::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    //inflate para el menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.activity_menu, menu)
+        return true
+    }
+
+
+    fun escucharCambiosdeDisponibilidad() {
+        myRef = database.getReference(PATH_USERS)
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val cursor = MatrixCursor(arrayOf("_id", "nombre", "apellido", "imagen", "email", "latitud", "longitud"))
+                var i = 0
+
+                for (singleSnapshot in dataSnapshot.children) {
+                    val myUser = singleSnapshot.getValue(Usuario::class.java)
+                    if (myUser != null) {
+                        val userKey = singleSnapshot.key
+                        val isAvailable = myUser.disponible
+                        if (isAvailable) {
+                            cursor.addRow(
+                                arrayOf(
+                                    i,
+                                    myUser.nombre,
+                                    myUser.apellido,
+                                    myUser.imageUrl,
+                                    myUser.email,
+                                    myUser.latitud,
+                                    myUser.longitud
+                                )
+                            )
+                            Toast.makeText(
+                                this@Inicio, "${myUser.nombre} se ha conectado", Toast.LENGTH_SHORT).show()
+                            i++
+                        } else {
+                            Toast.makeText(
+                                this@Inicio, "${myUser.nombre} se ha desconectado", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "error en la consulta", databaseError.toException())
+            }
+        })
     }
 }
